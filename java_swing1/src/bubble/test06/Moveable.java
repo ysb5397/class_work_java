5c66aee79dc9335cbed87b31f2269b92527edbd9
Yangsb <ysb5397@gmail.com>
2025-05-07 16:51:30 +0900

    5월 7일 커밋

---
+++ java_swing1/src/bubble/test06/Moveable.java
@@ -0,0 +1,13 @@
+package bubble.test06;
+
+public interface Moveable {
+    // public abstract 생략가능
+    void left();
+    void right();
+    void up();
+    default void down() {}
+
+    // 인터페이스의 모든 메서드는 추상 메서드이다.
+    // 단, default 구현 메서드를 제외하고!!
+//    default void a() {}
+}
---
+++ java_swing1/src/bubble/test06/Player.java
@@ -0,0 +1,242 @@
+package bubble.test06;
+
+import javax.swing.*;
+
+public class Player extends JLabel implements Moveable {
+
+    private int x;
+    private int y;
+
+    private ImageIcon playerR;
+    private ImageIcon playerL;
+
+    // 플레이어의 속도 상태
+    private final int SPEED = 4;
+    private final int JUMP_SPEED = 2;
+
+    // 플레이어의 움직인 상태
+    private boolean left;
+    private boolean right;
+    private boolean up;
+    private boolean down;
+
+    // 벽에 충돌한 상태
+    private boolean leftWallCrash;
+    private boolean rightWallCrash;
+
+    // 플레이어 방향 상태 (enum 타입 사용 법 1 - 선언 )
+    private PlayerWay playerWay;
+
+    // PlayerWay - getter 만 생성
+    public PlayerWay getPlayerWay() {
+        return playerWay;
+    }
+
+    @Override
+    public int getX() {
+        return x;
+    }
+
+    @Override
+    public int getY() {
+        return y;
+    }
+
+    public ImageIcon getPlayerR() {
+        return playerR;
+    }
+
+    public ImageIcon getPlayerL() {
+        return playerL;
+    }
+
+    public int getSPEED() {
+        return SPEED;
+    }
+
+    public int getJUMP_SPEED() {
+        return JUMP_SPEED;
+    }
+
+    public boolean isLeft() {
+        return left;
+    }
+
+    public boolean isRight() {
+        return right;
+    }
+
+    public boolean isUp() {
+        return up;
+    }
+
+    public boolean isDown() {
+        return down;
+    }
+
+    public boolean isLeftWallCrash() {
+        return leftWallCrash;
+    }
+
+    public boolean isRightWallCrash() {
+        return rightWallCrash;
+    }
+
+    public void setX(int x) {
+        this.x = x;
+    }
+
+    public void setY(int y) {
+        this.y = y;
+    }
+
+    public void setPlayerR(ImageIcon playerR) {
+        this.playerR = playerR;
+    }
+
+    public void setPlayerL(ImageIcon playerL) {
+        this.playerL = playerL;
+    }
+
+    public void setLeft(boolean left) {
+        this.left = left;
+    }
+
+    public void setRight(boolean right) {
+        this.right = right;
+    }
+
+    public void setUp(boolean up) {
+        this.up = up;
+    }
+
+    public void setDown(boolean down) {
+        this.down = down;
+    }
+
+    public void setLeftWallCrash(boolean leftWallCrash) {
+        this.leftWallCrash = leftWallCrash;
+    }
+
+    public void setRightWallCrash(boolean rightWallCrash) {
+        this.rightWallCrash = rightWallCrash;
+    }
+
+    public Player() {
+        initData();
+        setInitLayout();
+    }
+
+    private void initData() {
+        playerR = new ImageIcon("img/playerR.png");
+        playerL = new ImageIcon("img/playerL.png");
+        // 플레이어 초기 상태 설정
+        x = 55;
+        y = 535;
+
+        left = false;
+        right = false;
+        up = false;
+        down = false;
+    }
+
+    private void setInitLayout() {
+
+
+        setSize(50, 50);
+        setIcon(playerR);
+        setLocation(x, y);
+    }
+
+
+    @Override
+    public void left() {
+        // 클래스 이름으로 접근한다.
+        playerWay = PlayerWay.LEFT;
+        left = true;
+        new Thread(new Runnable() {
+            @Override
+            public void run() {
+                while (left) {
+                    setIcon(playerL);
+                    x = x - SPEED;
+                    setLocation(x, y);
+
+                    try {
+                        Thread.sleep(10);
+                    } catch (InterruptedException e) {
+                        throw new RuntimeException(e);
+                    }
+                } // end of while
+            } // end of run()
+        }).start();
+    }
+
+    @Override
+    public void right() {
+        playerWay = PlayerWay.RIGHT;
+        right = true; // 움직임 상태값 변경
+        // 익명 클래스 - thread.start() ---> run() 메서안에 구문 동작된다.
+        new Thread(new Runnable() {
+            @Override
+            public void run() {
+                while (right) {
+                    setIcon(playerR);
+                    x = x + SPEED;
+                    setLocation(x, y);
+                    try {
+                        Thread.sleep(10);
+                    } catch (InterruptedException e) {
+                        throw new RuntimeException(e);
+                    }
+                }
+            }
+        }).start();
+    }
+
+    @Override
+    public void up() {
+        up = true;
+        new Thread(new Runnable() {
+            @Override
+            public void run() {
+                for(int i = 0; i < 130 / JUMP_SPEED; i++) {
+                    y = y - JUMP_SPEED;
+                    setLocation(x, y);
+                    try {
+                        Thread.sleep(5);
+                    } catch (InterruptedException e) {
+                        throw new RuntimeException(e);
+                    }
+                } // end of for
+                up = false; // 상태값을 잘 다루어야 버그가 없다.
+                down();
+            }
+        }).start();
+
+
+    }
+
+    @Override
+    public void down() {
+        down = true;
+        new Thread(new Runnable() {
+            @Override
+            public void run() {
+
+                while (down) {
+                    y += JUMP_SPEED;
+                    setLocation(x, y);
+
+                    try {
+                        Thread.sleep(3);
+                    } catch (InterruptedException e) {
+                        throw new RuntimeException(e);
+                    }
+                } // end of while
+                down = false; // 상태값을 확실하게 처리하자.
+            }
+        }).start();
+
+    }
+}
