0e3181c115e0bb88ee6cc8cc7b96c43c656c8230
Yangsb <ysb5397@gmail.com>
2025-06-04 17:04:38 +0900

    lotto main 수정

--- java_io/src/ch04/lotto/LottoMain.java
+++ java_io/src/ch04/lotto/LottoMain.java
@@ -30,14 +30,15 @@ public class LottoMain {
         WriteNumberToFile writeNumberToFile = new WriteNumberToFile();
 
         while(true) {
-            System.out.println("로또 번호를 몇 회 생성하시겠습니까? 단, 횟수는 한 번에 10회 이하로 제한합니다.\n만약 종료 하고 싶다면 -1을 입력해주세요.");
+            System.out.println("로또 번호를 몇 회 생성하시겠습니까?\n만약 종료 하고 싶다면 -1을 입력해주세요.");
             try {
                 n = scanner.nextInt();
-                totalCount += n;
 
-                if (n > 10) {
-                    System.err.println("10회 이하로 입력해주세요!");
-                } else if (n == -1) {
+//                if (n > 10) {
+//                    System.err.println("10회 이하로 입력해주세요!");
+//                } else
+
+                if (n == -1) {
                     System.out.println("프로그램을 종료함에 따라 로또 번호 출력 횟수는 초기화 됩니다.");
                     break;
                 } else if (n != 0 && !(n < -1)) {
@@ -45,6 +46,7 @@ public class LottoMain {
                     // m은 로또 번호가 몇 번 생성되었는지를 의미
                     // totalCount는 로또 번호가 총 몇번 생성 되었는지 의미
                     // 이때, m은 다시 반환되어서 프로그램이 종료하기 전까지 기록된다.
+                    totalCount += n;
                     while (!(m >= totalCount)) {
                         m = genLottoNumber.generateLottoNumber(n, m);
                         m = writeNumberToFile.writeToFile(n, m);
